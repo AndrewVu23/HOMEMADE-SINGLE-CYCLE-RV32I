8efddac62599b80bf27e080b5e49fdd8377d9436
Andrew Vu <105542810+AndrewVu23@users.noreply.github.com>
2026-02-09 23:09:35 -0500

    Add comments to PC_plus_4 module
    Updated PC_plus_4 module to include comments for clarity.

--- Components/PC_plus_4.v
+++ Components/PC_plus_4.v
@@ -3,5 +3,7 @@ module PC_plus_4(
   input logic [31:0] PC,
   output logic [31:0] PC_plus_4
 );
+//A Program Counter that automatically moves to the next
+//address (increasing by 4) reading the current address
   assign PC_plus_4 = (reset) ? 32'h0 : PC + 32'd4;
 endmodule
