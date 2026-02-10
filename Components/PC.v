5468b99810e481ea2614524dc204179018a7177f
Andrew Vu <105542810+AndrewVu23@users.noreply.github.com>
2026-02-09 23:05:51 -0500

    Fix PC assignment logic in the Program Counter

--- Components/PC.v
+++ Components/PC.v
@@ -3,7 +3,10 @@ module PC(
   input logic [31:0] PC_next,
   output logic [31:0] PC
 );
+//A Program Counter which points to the address
+//of the curret instruction and taking the next
+//address on each rising edge 
 always @(posedge clk) begin
-  PC_next <= (reset) ? 32'h0 : PC;
+  PC <= (reset) ? 32'h0 : PC_next;
 end
 endmodule
