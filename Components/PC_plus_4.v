85353c370f8f4654c50f8e7ae5baaf45a6603bc1
Andrew Vu <105542810+AndrewVu23@users.noreply.github.com>
2026-02-09 22:32:33 -0500

    Fix Combinational Logic

--- Components/PC_plus_4.v
+++ Components/PC_plus_4.v
@@ -1,9 +1,7 @@
 module PC_plus_4(
-  input logic clk, reset,
+  input reset,
   input logic [31:0] PC,
   output logic [31:0] PC_plus_4
 );
-always @(posedge clk) begin
-  PC_plus_4 <= (reset) ? 32'h0 : PC + 32'd4;
-end
+  assign PC_plus_4 = (reset) ? 32'h0 : PC + 32'd4;
 endmodule
