f603d93ab46612433b7a128c8af8ebcc3076f498
Andrew Vu <105542810+AndrewVu23@users.noreply.github.com>
2026-02-10 23:49:55 -0500

    Refactor ALU operations to use non-blocking assignments

--- Components/ALU.v
+++ Components/ALU.v
@@ -15,14 +15,14 @@ assign signed_B = $signed(B);
 //ALU Operations
 always @(*) begin
   case(ALUControl)
-    3'b000: ALUResult <= signed_A + signed_B; //Addition
-    3'b001: ALUResult <= signed_A - signed_B; //Subtraction
-    3'b101: ALUResult <= (signed_A < signed_B) ? 32'h1 : 32'h0; //Set (if) Less Than
-    3'b011: ALUResult <= A | B; //OR
-    3'b010: ALUResult <= A & B; //AND
-    default: ALUResult <= 32'h0; //Initialize result to be 0
+    3'b000: ALUResult = signed_A + signed_B; //Addition
+    3'b001: ALUResult = signed_A - signed_B; //Subtraction
+    3'b101: ALUResult = (signed_A < signed_B) ? 32'h1 : 32'h0; //Set (if) Less Than
+    3'b011: ALUResult = A | B; //OR
+    3'b010: ALUResult = A & B; //AND
+    default: ALUResult = 32'h0; //Initialize result to be 0
   endcase
 
-  flag <= (ALUResult == 32'h0) ? 1 : 0; //Set a zero flag for 'beq' Instruction
+  flag = (ALUResult == 32'h0) ? 1 : 0; //Set a zero flag for 'beq' Instruction
 end
 endmodule
