38588295f491881b4f50e1be930a8d8fe4a950ba
Andrew Vu <105542810+AndrewVu23@users.noreply.github.com>
2026-02-11 10:25:15 -0500

    Set instruction memory values in testbench
    Initialize instruction memory for easier debugging.

--- Testbenches/Instr_Fetch_tb.v
+++ Testbenches/Instr_Fetch_tb.v
@@ -5,6 +5,14 @@ module Instr_Fetch_tb;
   Instr_Fetch dut(.*);
 
   initial begin
+    //Set the value of each array of the 1024-word array so that
+    //the Instruction Address matches the Instruction Data, making
+    //debugging easier.
+    integer i;
+    for (i = 0; i < 1023; i = i + 1) begin
+      Instr_Mem.instruction_mem[i] = i * 4;
+    end
+    PC.PC = 0;
     reset = 0; clk = 0;
     #5 reset = 1;
     #10 reset = 0;
