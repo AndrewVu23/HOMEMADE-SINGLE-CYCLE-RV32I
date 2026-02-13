11ca75387fc09f839b9db1635d50f83a796c70d2
Andrew Vu <105542810+AndrewVu23@users.noreply.github.com>
2026-02-13 18:09:03 -0500

    Initiate the value for Reg 0 (x0)

--- Components/Reg_File.v
+++ Components/Reg_File.v
@@ -12,12 +12,13 @@ reg [31:0] Registers [31:0];
 //The data at address 1 and 2 needs to be read at all time
 //so that's why we use combinational logic here to read
 //the data at the respective registers
-assign read_address_1 = (reset) ? 32'h0 : Registers[address_1];
-assign read_address_2 = (reset) ? 32'h0 : Registers[address_2];
+assign read_address_1 = (reset == 1 || address_1 == 0) ? 32'h0 : Registers[address_1];
+assign read_address_2 = (reset == 1 || address_2 == 0) ? 32'h0 : Registers[address_2];
 
 //Data can only be written when we allow it to write (write_data = HIGH)
 //Register 0 (x0) is hardwired to 0, so we can't write into that register
 always @(posedge clk) begin
-  if (reg_write && address_data != 5'b0) Registers[address_data] <= write_data;
+  if (reg_write == 1 && address_data !== 0) Registers[address_data] <= write_data;
 end
+  
 endmodule
