9e98a07e43f348de552f17d9ab494d0fd5ffc4f1
Andrew Vu <105542810+AndrewVu23@users.noreply.github.com>
2026-02-12 21:45:39 -0500

    Add Sign Extension Module

---
+++ Components/Sign_Ext.v
@@ -0,0 +1,14 @@
+module Sign_Ext(
+  input logic [1:0] ImmSrc,
+  input logic [31:0] instruction,
+  output logic [31:0] ImmExt
+);
+  always_comb begin
+    case(ImmSrc)
+      2'b00: ImmExt = {{20{instruction[31]}}, instruction[31:20]};
+      2'b01: ImmExt = {{20{instruction[31]}}, instruction[31:25], instruction[11:7]};
+      2'b10: ImmExt = {{20{instruction[31]}}, instruction[7], instruction[30:25], instruction[11:8], 1'b0};
+      default: ImmExt = 32'bx;
+    endcase
+  end
+endmodule
