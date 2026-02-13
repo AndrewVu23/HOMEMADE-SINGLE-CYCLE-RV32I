79f003c7886fc7306c7bdbbe83ec445c178ae9e0
Andrew Vu <105542810+AndrewVu23@users.noreply.github.com>
2026-02-12 21:57:54 -0500

    Implement other instructions

--- Components/Instrucitons.v
+++ Components/Instrucitons.v
@@ -1,10 +1,9 @@
-module R_type(
+module Instructions(
   input clk, reset
 );
-//R-type datapath, which consists of:
-//Instruction Fetch, ALU, ALU Decoder, and Register File
-  
-wire [31:0] ALU_Reg, read_address_1, read_address_2, instruction;
+
+//Contain R, S, B, I Instructions
+wire [31:0] ALU_Reg, read_address_1, read_address_2, instruction, ImmExt, B;
 wire [2:0] ALUControl;
 
 Instr_Fetch Instr_Fetch(.*);
@@ -21,9 +20,15 @@ Reg_File Reg_File(
   .write_data(ALU_Reg)
 );
 
+Sign_Ext Sign_Ext(
+  .instruction(instruction),
+  .ImmSrc(1'b00), //hardcoded for now
+  .ImmExt(ImmExt)
+);
+
 ALU_Decoder ALU_Decoder(
   .op5(instruction[5]),
-  .funct_7_5(instruction[30]),
+  .funct7_5(instruction[30]),
   .funct3(instruction[14:12]),
   .ALUControl(ALUControl),
   .ALUOp(2'b10)
@@ -31,10 +36,17 @@ ALU_Decoder ALU_Decoder(
 
 ALU ALU(
   .A(read_address_1),
-  .B(read_address_2),
+  .B(B),
   .ALUResult(ALU_Reg),
   .ALUControl(ALUControl),
   .flag()
+);
+
+Mux2 Mux2(
+  .ALUSrc(1'b1), //hardcoded for now
+  .read_address_2(read_address_2),
+  .ImmExt(ImmExt),
+  .B(B)
 );
 
 endmodule
