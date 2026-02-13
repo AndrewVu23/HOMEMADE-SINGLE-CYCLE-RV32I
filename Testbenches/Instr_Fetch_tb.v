3736ded839eea36150342360e79a352b8304452e
Andrew Vu <105542810+AndrewVu23@users.noreply.github.com>
2026-02-13 17:15:20 -0500

    Add Self-checking operation for Instruction Fetch Module
    Added expected_val to compare with instruction output.

--- Testbenches/Instr_Fetch_tb.v
+++ Testbenches/Instr_Fetch_tb.v
@@ -1,18 +1,30 @@
 module Instr_Fetch_tb;
   logic clk, reset;
   logic [31:0] instruction;
-  
+  logic [31:0] expected_val;
+
   Instr_Fetch dut(.*);
 
+  integer i;
   initial begin
     //Set the value of each array of the 1024-word array so that
     //the Instruction Address matches the Instruction Data, making
     //debugging easier.
-    integer i;
     for (i = 0; i < 1023; i = i + 1) begin
       dut.Instr_Mem.instruction_mem[i] = i * 4;
+
+      @(posedge clk);
+
+      #1 expected_val = dut.PC_wire;
+
+      if (instruction !== expected_val) $display("FAILED. Expected Values: %d. Got: %d", expected_val, instruction);
+      else $display("PASSED");
     end
-    PC.PC = 0;
+  end
+
+  initial begin
+    $dumpfile("waveform.vcd");
+    $dumpvars(0, Instr_Fetch_tb);
     reset = 0; clk = 0;
     #5 reset = 1;
     #10 reset = 0;
