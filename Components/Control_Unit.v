4ab7778c125ce605b2eb6226bde80357d60a54b5
Andrew Vu <105542810+AndrewVu23@users.noreply.github.com>
2026-02-13 21:44:59 -0500

    Modify Control_Unit to include Jump and PCSrc signals

--- Components/Control_Unit.v
+++ Components/Control_Unit.v
@@ -1,34 +1,42 @@
 module Control_Unit(
   input logic [6:0] opcode,
-  output logic reg_write, ALUSrc, MemWrite, ResultSrc, Branch, 
-  output logic [1:0] ImmSrc, ALUOp
+  input flag,
+  output logic reg_write, ALUSrc, MemWrite, Branch, Jump, PCSrc
+  output logic [1:0] ImmSrc, ALUOp, ResultSrc
 );
   always @(*) begin
     case(opcode)
       7'b0000011: begin
-        reg_write = 1; ALUSrc = 1; MemWrite = 0; ResultSrc = 1; Branch = 0;
+        reg_write = 1; ALUSrc = 1; MemWrite = 0; ResultSrc = 2'b01; Branch = 0; Jump = 0;
         ImmSrc = 2'b00; ALUOp = 2'b00;
       end
       7'b0100011: begin
-        reg_write = 0; ALUSrc = 1; MemWrite = 1; ResultSrc = 1'bx; Branch = 0;
+        reg_write = 0; ALUSrc = 1; MemWrite = 1; ResultSrc = 1'bx; Branch = 0; Jump = 0;
         ImmSrc = 2'b01; ALUOp = 2'b00;
       end
       7'b0110011: begin
-        reg_write = 1; ALUSrc = 0; MemWrite = 0; ResultSrc = 0; Branch = 0;
+        reg_write = 1; ALUSrc = 0; MemWrite = 0; ResultSrc = 2'b00; Branch = 0; Jump = 0;
         ImmSrc = 2'bxx; ALUOp = 2'b10;
       end
       7'b1100011: begin
-        reg_write = 0; ALUSrc = 0; MemWrite = 0; ResultSrc = 1'bx; Branch = 1;
+        reg_write = 0; ALUSrc = 0; MemWrite = 0; ResultSrc = 2'bxx; Branch = 1; Jump = 0;
         ImmSrc = 2'b10; ALUOp = 2'b01;
       end
       7'b0010011: begin
-        reg_write = 1; ALUSrc = 1; MemWrite = 0; ResultSrc = 0; Branch = 0;
+        reg_write = 1; ALUSrc = 1; MemWrite = 0; ResultSrc = 2'b00; Branch = 0; Jump = 0;
         ImmSrc = 2'b00; ALUOp = 2'b10;
       end
+      7'b1101111: begin
+        reg_write = 1; ALUSrc = 1'bx; MemWrite = 0; ResultSrc = 2'b10; Branch = 0; Jump = 1;
+        ImmSrc = 2'b11; ALUOp = 2'bxx;
+      end
       default: begin
-        reg_write = 0; ALUSrc = 0; MemWrite = 0; ResultSrc = 0; Branch = 0;
+        reg_write = 0; ALUSrc = 0; MemWrite = 0; ResultSrc = 0; Branch = 0; Jump = 0;
         ImmSrc = 2'b00; ALUOp = 2'b00;
       end
     endcase
   end
+  assign jump_a = Branch & flag;
+  assign PCSrc = jump_a ^ Jump;
+
 endmodule
