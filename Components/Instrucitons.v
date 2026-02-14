decddeef273c9f0f106de8bc5a90c864bb1daaf1
Andrew Vu <105542810+AndrewVu23@users.noreply.github.com>
2026-02-13 21:48:19 -0500

    Change Instructions module to support R,S,I,B,J type Instructions

--- Components/Instrucitons.v
+++ Components/Instrucitons.v
@@ -1,28 +1,66 @@
 module Instructions(
   input clk, reset
 );
+logic [31:0] ALUResult, read_address_1, read_address_2, instruction, ImmExt, B, Result, read_data, PC, PC_plus_4, PCTarget, PC_next;
+logic [2:0] ALUControl;
+logic [1:0] ALUOp, ImmSrc;
+logic reg_write, ALUSrc, MemWrite, ResultSrc, Branch, PCSrc, flag;
 
-//Contain R, S, B, I Instructions
-wire [31:0] ALU_Reg, read_address_1, read_address_2, instruction, ImmExt, B;
-wire [2:0] ALUControl;
+PC PC(
+  .clk(clk),
+  .reset(reset),
+  .PC(PC),
+  .PC_next(PC_next)
+);
 
-Instr_Fetch Instr_Fetch(.*);
+PC_plus_4 PC_plus_4(
+  .reset(reset),
+  .PC(PC),
+  .PC_plus_4(PC_plus_4)
+);
+
+Instr_Mem Instr_Mem(
+  .reset(reset),
+  .address(PC),
+  .instruction_out(instruction)
+);
 
 Reg_File Reg_File(
   .clk(clk),
   .reset(reset),
-  .reg_write(1'b1),
+  .reg_write(reg_write),
   .address_1(instruction[19:15]),
   .address_2(instruction[24:20]),
   .address_data(instruction[11:7]),
   .read_address_1(read_address_1),
   .read_address_2(read_address_2),
-  .write_data(ALU_Reg)
+  .write_data(Result)
+);
+
+Control_Unit Control_Unit(
+  .opcode(instruction[6:0]),
+  .reg_write(.reg_write),
+  .ALUSrc(ALUSrc),
+  .MemWrite(MemWrite),
+  .ResultSrc(ResultSrc),
+  .Branch(Branch),
+  .ImmSrc(ImmSrc),
+  .ALUOp(ALUOp),
+  .flag(flag),
+  .PCSrc(PCSrc)
+);
+
+Data_Mem Data_Mem(
+  .clk(clk),
+  .MemWrite(MemWrite),
+  .address(ALUResult),
+  .write_data(read_address_2),
+  .read_data(read_data)
 );
 
 Sign_Ext Sign_Ext(
   .instruction(instruction),
-  .ImmSrc(1'b00), //hardcoded for now
+  .ImmSrc(ImmSrc),
   .ImmExt(ImmExt)
 );
 
@@ -31,22 +69,43 @@ ALU_Decoder ALU_Decoder(
   .funct7_5(instruction[30]),
   .funct3(instruction[14:12]),
   .ALUControl(ALUControl),
-  .ALUOp(2'b10)
+  .ALUOp(ALUOp)
 );
 
 ALU ALU(
   .A(read_address_1),
   .B(B),
-  .ALUResult(ALU_Reg),
+  .ALUResult(ALUResult),
   .ALUControl(ALUControl),
-  .flag()
+  .flag(flag)
+);
+
+PC_Target PC_Target(
+  .PCTarget(PCTarget),
+  .ImmExt(ImmExt),
+  .PC(PC)
+);
+
+Mux1 Mux1(
+  .PCTarget(PCTarget),
+  .PC_plus_4(PC_plus_4),
+  .PC_next(PC_next),
+  .PCSrc(PCSrc)
 );
 
 Mux2 Mux2(
-  .ALUSrc(1'b1), //hardcoded for now
+  .ALUSrc(ALUSrc),
   .read_address_2(read_address_2),
   .ImmExt(ImmExt),
   .B(B)
+);
+
+Mux3 Mux3(
+  .PC_plus_4(PC_plus_4),
+  .read_data(read_data),
+  .ALUResult(ALUResult),
+  .ResultSrc(ResultSrc),
+  .Result(Result)
 );
 
 endmodule
