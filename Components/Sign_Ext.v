f6c03670dd5613f5682e59d58998d85418ee2f53
Andrew Vu <105542810+AndrewVu23@users.noreply.github.com>
2026-02-13 22:03:22 -0500

    Update Sign_Ext.v

--- Components/Sign_Ext.v
+++ Components/Sign_Ext.v
@@ -3,7 +3,7 @@ module Sign_Ext(
   input logic [31:0] instruction,
   output logic [31:0] ImmExt
 );
-  always_comb begin
+  always @(*) begin
     case(ImmSrc)
       2'b00: ImmExt = {{20{instruction[31]}}, instruction[31:20]};
       2'b01: ImmExt = {{20{instruction[31]}}, instruction[31:25], instruction[11:7]};
