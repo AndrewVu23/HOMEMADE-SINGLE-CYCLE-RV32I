5f54a3f41d4ac8f5145c5399de08c62a1fabb65e
Andrew Vu <105542810+AndrewVu23@users.noreply.github.com>
2026-02-13 21:05:37 -0500

    Refactor Mux3 to support J-type instruction

--- Components/Mux3.v
+++ Components/Mux3.v
@@ -1,9 +1,15 @@
 //A MUX between Data Memory and ALU to enable data storage
 module Mux3(
-  input logic [31:0] ALUResult, read_data,
-  input logic ResultSrc,
+  input logic [31:0] ALUResult, read_data, PC_plus_4,
+  input logic [1:0] ResultSrc,
   output logic [31:0] Result
 );
-assign Result = (ResultSrc) ? read_data : ALUResult;
+  always @(*) begin
+    case(ResultSrc)
+      2'b00: Result = ALUResult;
+      2'b01: Result = read_data;
+      2'b10: Result = PC_plus_4;
+    endcase
+  end
 
 endmodule
