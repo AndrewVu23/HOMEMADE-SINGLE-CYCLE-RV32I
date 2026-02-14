9c7047ee10850177359397c212424f92d444c2de
Andrew Vu <105542810+AndrewVu23@users.noreply.github.com>
2026-02-13 21:47:25 -0500

    Add J-instruction for Sign Extender

--- Components/Sign_Ext.v
+++ Components/Sign_Ext.v
@@ -8,6 +8,7 @@ module Sign_Ext(
       2'b00: ImmExt = {{20{instruction[31]}}, instruction[31:20]};
       2'b01: ImmExt = {{20{instruction[31]}}, instruction[31:25], instruction[11:7]};
       2'b10: ImmExt = {{20{instruction[31]}}, instruction[7], instruction[30:25], instruction[11:8], 1'b0};
+      2'b11: ImmExt = {{12{instruction[31]}}, instruction[19:12], instruction[20], instruction[30:21], 1'b0};
       default: ImmExt = 32'bx;
     endcase
   end
