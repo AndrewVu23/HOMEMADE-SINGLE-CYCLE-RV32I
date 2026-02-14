f0d482a86eb3c4eac162145ddfab008d590f4444
Andrew Vu <105542810+AndrewVu23@users.noreply.github.com>
2026-02-13 22:02:58 -0500

    Update Instrucitons.v & Rename the module to fix naming mismatching

--- Components/Instrucitons.v
+++ Components/Instrucitons.v
@@ -3,29 +3,29 @@ module Instructions(
 );
 logic [31:0] ALUResult, read_address_1, read_address_2, instruction, ImmExt, B, Result, read_data, PC, PC_plus_4, PCTarget, PC_next;
 logic [2:0] ALUControl;
-logic [1:0] ALUOp, ImmSrc;
-logic reg_write, ALUSrc, MemWrite, ResultSrc, Branch, PCSrc, flag;
+logic [1:0] ALUOp, ImmSrc, ResultSrc;
+logic reg_write, ALUSrc, MemWrite, Branch, PCSrc, flag;
 
-PC PC(
+PC PC_Module(
   .clk(clk),
   .reset(reset),
   .PC(PC),
   .PC_next(PC_next)
 );
 
-PC_plus_4 PC_plus_4(
+PC_plus_4 PC_plus_4_Module(
   .reset(reset),
   .PC(PC),
   .PC_plus_4(PC_plus_4)
 );
 
-Instr_Mem Instr_Mem(
+Instr_Mem Instr_Mem_Module(
   .reset(reset),
   .address(PC),
   .instruction_out(instruction)
 );
 
-Reg_File Reg_File(
+Reg_File Reg_File_Module(
   .clk(clk),
   .reset(reset),
   .reg_write(reg_write),
@@ -37,9 +37,9 @@ Reg_File Reg_File(
   .write_data(Result)
 );
 
-Control_Unit Control_Unit(
+Control_Unit Control_Unit_Module(
   .opcode(instruction[6:0]),
-  .reg_write(.reg_write),
+  .reg_write(reg_write),
   .ALUSrc(ALUSrc),
   .MemWrite(MemWrite),
   .ResultSrc(ResultSrc),
@@ -50,7 +50,7 @@ Control_Unit Control_Unit(
   .PCSrc(PCSrc)
 );
 
-Data_Mem Data_Mem(
+Data_Mem Data_Mem_Module(
   .clk(clk),
   .MemWrite(MemWrite),
   .address(ALUResult),
@@ -58,13 +58,13 @@ Data_Mem Data_Mem(
   .read_data(read_data)
 );
 
-Sign_Ext Sign_Ext(
+Sign_Ext Sign_Ext_Module(
   .instruction(instruction),
   .ImmSrc(ImmSrc),
   .ImmExt(ImmExt)
 );
 
-ALU_Decoder ALU_Decoder(
+ALU_Decoder ALU_Decoder_Module(
   .op5(instruction[5]),
   .funct7_5(instruction[30]),
   .funct3(instruction[14:12]),
@@ -72,7 +72,7 @@ ALU_Decoder ALU_Decoder(
   .ALUOp(ALUOp)
 );
 
-ALU ALU(
+ALU ALU_Module(
   .A(read_address_1),
   .B(B),
   .ALUResult(ALUResult),
@@ -80,27 +80,27 @@ ALU ALU(
   .flag(flag)
 );
 
-PC_Target PC_Target(
+PC_Target PC_Target_Module(
   .PCTarget(PCTarget),
   .ImmExt(ImmExt),
   .PC(PC)
 );
 
-Mux1 Mux1(
+Mux1 Mux1_Module(
   .PCTarget(PCTarget),
   .PC_plus_4(PC_plus_4),
   .PC_next(PC_next),
   .PCSrc(PCSrc)
 );
 
-Mux2 Mux2(
+Mux2 Mux2_Module(
   .ALUSrc(ALUSrc),
   .read_address_2(read_address_2),
   .ImmExt(ImmExt),
   .B(B)
 );
 
-Mux3 Mux3(
+Mux3 Mux3_Module(
   .PC_plus_4(PC_plus_4),
   .read_data(read_data),
   .ALUResult(ALUResult),
